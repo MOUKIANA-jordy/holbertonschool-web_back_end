aa4818809d21b9f9cacc8d37aa3c9435f1001dc6
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-07-18 13:11:21 +0200

    dependance

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2012,
     sourceType: 'module',
   },
   plugins: ['jest'],
