8c86d47f1ceb8bb00c5fa5f0251a7a6bd0bad8c6
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-03 16:31:22 +0200

    code

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -32,4 +32,3 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
         bool: True if password matches, False otherwise.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
