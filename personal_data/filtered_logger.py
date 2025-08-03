ba7fa2a46c5f6d7912911fb72e951304044f392f
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-03 13:33:08 +0200

    add modif

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,18 @@ def filter_datum(
     fields: List[str], redaction: str,
     message: str, separator: str
 ) -> str:
+    """
+    Obfuscate fields in a log message.
+
+    Args:
+        fields (List[str]): List of field names to redact.
+        redaction (str): The string used to replace field values.
+        message (str): The original log message.
+        separator (str): The character separating fields in the message.
+
+    Returns:
+        str: The log message with specified fields obfuscated.
+    """
     pattern = fr"({'|'.join(fields)})=.*?{separator}"
     return re.sub(
         pattern,
