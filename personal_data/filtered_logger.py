77d363aa9edae5a853653f8695598a42d6e41742
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-03 15:45:06 +0200

    add ajout

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3#!/usr/bin/env python3
+#!/usr/bin/env python3
 """Module for filtering and redacting sensitive log information."""
 import re
 import logging
@@ -36,18 +36,21 @@ class RedactingFormatter(logging.Formatter):
             super().format(record),
             self.SEPARATOR
         )
-    PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-    def get_logger() -> logging.Logger:
-        """Creates and configures a logger for user data."""
-        logger = logging.getLogger("user_data")
-        logger.setLevel(logging.INFO)
-        logger.propagate = False
+# Ces deux lignes doivent être en dehors de la classe RedactingFormatter
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-        logger.addHandler(stream_handler)
 
-        return logger
+def get_logger() -> logging.Logger:
+    """Creates and configures a logger for user data."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
 
