4eaea33f4203ca71391fcd13b43ed9f1ce9b3d35
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-03 13:29:33 +0200

    add modif

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-"""Masquer certains champs d’un message de log à l’aide d’une expression régulière"""
+"""Masquer certains champs d’un message de log"""
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+    fields: List[str], redaction: str,
+    message: str, separator: str
+) -> str:
     pattern = fr"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+    return re.sub(
+        pattern,
+        lambda m: f"{m.group(1)}={redaction}{separator}",
+        message
+    )
