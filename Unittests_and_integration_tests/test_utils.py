7ee72fc2948176860b9186f21d884429f6f4b624
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-11 01:52:18 +0200

    second commit

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -13,8 +13,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test correct access to nested maps"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test KeyError is raised with correct message"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[len(cm.exception.args[0]) and 0]))
 
-if __name__ == "__main__":
-    unittest.main()
