b6d9a8e5c207a707f06ac0192cbfbfa270f8bc4d
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-08-11 20:46:02 +0200

    add unittest

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test correct access to nested maps"""
+        """Test that access_nested_map returns expected result"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -21,8 +21,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Test KeyError is raised with correct message"""
-        with self.assertRaises(KeyError) as cm:
+        """Test that KeyError is raised with correct message"""
+        with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), repr(path[len(cm.exception.args[0]) and 0]))
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
