e485e4c64d4e04f73c2c427c01d3bd2cfa3d74a6
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-22 17:49:30 +0200

    add fil

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -94,3 +94,26 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+def filter_datum(fields, redaction, message, separator):
+    """Remplace les valeurs des champs sensibles par la redaction"""
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
