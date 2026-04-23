45e3616cf8cb11407f828750ae8a53d58b012784
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 18:56:53 +0200

    add files

---
+++ Session_authentication/api/v1/views/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """Return API status"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """Return stats"""
+    from models.user import User
+    stats = {'users': User.count()}
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route():
+    """Trigger 401"""
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_route():
+    """Trigger 403"""
+    abort(403)
