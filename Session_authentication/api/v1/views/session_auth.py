2e6e6bf4f6d67a394b533adb74afc6fcbba42d35
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 21:37:51 +0200

    add files

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -40,7 +40,11 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE'],
+    strict_slashes=False
+)
 def logout():
     """Logout user by destroying session"""
     from api.v1.app import auth
