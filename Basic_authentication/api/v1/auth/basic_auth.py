6a747af0954667649097e75abd512f7d1febf382
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 17:25:27 +0200

    add files

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic authentication module"""
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Decode a Base64 authorization header"""
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode("utf-8")
+        except Exception:
+            return None
