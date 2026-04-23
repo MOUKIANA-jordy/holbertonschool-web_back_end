afb6a2884d8d0eda551e31f813bbe563867112d7
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 20:09:43 +0200

    add files

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 """Session authentication module"""
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class"""
 
-    def require_auth(self, path: str, excluded_paths: list) -> bool:
-        """Determine if auth is required"""
-        return super().require_auth(path, excluded_paths)
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
