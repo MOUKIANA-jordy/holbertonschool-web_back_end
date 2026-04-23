dd205df04f3d3a40818534377b742bae1dd4421a
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 20:26:42 +0200

    add files

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Auth module for managing API authentication"""
 
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -30,3 +31,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Return the current user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request"""
+        if request is None:
+            return None
+
+        session_name = getenv("SESSION_NAME")
+        return request.cookies.get(session_name)
