71418eab3ec29bc944841b4097fa3ec58f1e3117
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 19:55:54 +0200

    add files

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -43,6 +43,9 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """Filter all incoming requests"""
+    
+    print("AUTH TYPE:", type(auth))  
+    
     if auth is None:
         return
 
