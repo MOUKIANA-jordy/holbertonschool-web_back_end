a8e3594a7ef740e1a82a69637d7ac8232aaff757
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 16:13:48 +0200

    add files

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -18,6 +18,11 @@ def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     # Charger les utilisateurs depuis le fichier AVANT de démarrer
