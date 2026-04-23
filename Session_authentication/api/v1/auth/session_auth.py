cc3b05124f8f9d2932019caf44768bdee1b48379
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 19:26:14 +0200

    add files

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -6,4 +6,7 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """Session authentication class"""
-    pass
+
+    def require_auth(self, path: str, excluded_paths: list) -> bool:
+        """Determine if auth is required"""
+        return super().require_auth(path, excluded_paths)
