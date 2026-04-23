e4554276bb85ee1c8b627dc645bf65a976099115
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 12:55:57 +0200

    add app

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -20,10 +20,10 @@ babel = Babel()
 
 def get_locale():
     """Select best language"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel.init_app(app, locale_selector=get_locale)
+    return (
+        request.accept_languages.best_match(app.config["LANGUAGES"])
+        or app.config["BABEL_DEFAULT_LOCALE"]
+    )
 
 
 @app.route("/")
