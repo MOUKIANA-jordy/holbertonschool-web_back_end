e43f14cba2b72acc3ed022e14bb6157e68365d39
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 16:49:32 +0200

    add files

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if path requires authentication"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # rendre path slash tolerant
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded in excluded_paths:
+            if path == excluded:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return the authorization header"""
