198a2436000f2037208d9c3366fd527b884f51be
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-23 21:20:14 +0200

    add files

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -14,34 +14,38 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # 🔴 email missing
     if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
 
-    # 🔴 password missing
     if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
 
-    # 🔍 search user
     users = User.search({"email": email})
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
     user = users[0]
 
-    # 🔴 wrong password
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    # 🔥 créer session
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
 
-    # 📦 réponse JSON
     response = make_response(jsonify(user.to_json()))
 
-    # 🍪 set cookie
     session_name = getenv("SESSION_NAME")
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout user by destroying session"""
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
