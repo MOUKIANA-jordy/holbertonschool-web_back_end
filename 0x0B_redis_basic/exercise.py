ed8445165607f99a7f155533431eaacb49f2a5c0
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-08-26 16:55:42 +0200

    add files

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Provide a Redis cache with call-counting support."""
+"""Provide a Redis cache with call counting and history."""
 
 import uuid
 from functools import wraps
@@ -13,11 +13,30 @@ def count_calls(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Increment the call counter and call the original method."""
+        """Increment the counter before calling the method."""
         key = method.__qualname__
         self._redis.incr(key)
-
         return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Store the inputs and outputs of the decorated method."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Record inputs, execute the method and record its output."""
+        inputs_key = "{}:inputs".format(method.__qualname__)
+        outputs_key = "{}:outputs".format(method.__qualname__)
+
+        self._redis.rpush(inputs_key, str(args))
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(outputs_key, output)
+
+        return output
 
     return wrapper
 
@@ -31,6 +50,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis and return its generated key."""
         key = str(uuid.uuid4())
